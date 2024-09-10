532aa2de79958a7c36397925359c71ecb174451d
SandraRadz <oleksandraradzievska@gmail.com>
2024-09-10 19:36:23 +0300

    implement todolist logic; improve README.md

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,7 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("hello.urls")),
+    path("todolist/", include("todolist.urls")),
     path("admin/", admin.site.urls),
+    path("", include("hello.urls")),
 ]
 
--- mysite/todolist/urls.py
+++ mysite/todolist/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from todolist import views
 
+
+app_name = 'todolist'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='tasks_list'),
+    path('add/', views.add, name='add_task')
 ]
 
